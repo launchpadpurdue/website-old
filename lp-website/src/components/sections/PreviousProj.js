361a0bf2a670d5c940de8bf2fae9843c50e994e7
Joseph Seohyun Lee <89218792+lee3296@users.noreply.github.com>
2022-08-14 13:32:35 -0400

    updated with MaterialUI buttons

--- lp-website/src/components/sections/PreviousProj.js
+++ lp-website/src/components/sections/PreviousProj.js
@@ -1,9 +1,10 @@
 import Card from './Card';
 import classes from './ProjectItem.module.css';
 import ProjectList from './ProjectList';
-//import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-//import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-//import { Button } from '@mui/material';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { Button } from '@mui/material';
+import { IconButton } from '@mui/material';
 
 const DUMMY_DATA = [
     {
@@ -40,18 +41,24 @@ function PreviousProj() {
         <div>
             <center><h1>Previous projects</h1></center>
             <center><h3>Fall 2022</h3></center>
-              <div className={classes.container}>
-                <div className = {classes.row}>
-                  <ProjectList projects={DUMMY_DATA} />
-                </div>
+
+            <div className={classes.container}>
+              <IconButton color="primary" aria-label="previous">
+                <ArrowBackIosNewIcon />
+              </IconButton>
+  
+            
+              <div className = {classes.row}>
+                <ProjectList projects={DUMMY_DATA} />
               </div>
 
-            <center><h3>Fall 2021</h3></center>
-              <div className={classes.container}>
-                <div className = {classes.row}>
-                  <ProjectList projects={DUMMY_DATA} />
-                </div>
-              </div>
+              <IconButton color="primary" aria-label="next">
+                <ArrowForwardIosIcon />
+              </IconButton>
+            </div>
+
+            
+
         </div>
     );
   }
