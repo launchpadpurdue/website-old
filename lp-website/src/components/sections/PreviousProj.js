c4502d098ef538ecffbb4b117bd6b74e0889b5aa
Joseph Seohyun Lee <89218792+lee3296@users.noreply.github.com>
2022-08-14 12:25:09 -0400

    init commit with new branch
    created a basic layout for the previous projects section. Added some css files for the project card, feel free to move the files to somewhere more convenient, if necessary.

--- lp-website/src/components/sections/PreviousProj.js
+++ lp-website/src/components/sections/PreviousProj.js
@@ -1,7 +1,57 @@
+import Card from './Card';
+import classes from './ProjectItem.module.css';
+import ProjectList from './ProjectList';
+//import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+//import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+//import { Button } from '@mui/material';
+
+const DUMMY_DATA = [
+    {
+        id: 'm1',
+        title: 'E-Plan with Dart and Firebase',
+        image:
+          'https://i.imgur.com/1arjnNv.jpg',
+        address: 'Created during Fall 2021 of LaunchPad Purdue',
+        description:
+          'This app is a planner app that allows you to keep track of your progress and goals you have set. UI used with Dart is Flutter.',
+      },
+      {
+        id: 'm1',
+        title: 'E-Plan with Dart and Firebase',
+        image:
+          'https://i.imgur.com/1arjnNv.jpg',
+        address: 'Created during Fall 2021 of LaunchPad Purdue',
+        description:
+          'This app is a planner app that allows you to keep track of your progress and goals you have set. UI used with Dart is Flutter.',
+      },
+      {
+        id: 'm1',
+        title: 'E-Plan with Dart and Firebase',
+        image:
+          'https://i.imgur.com/1arjnNv.jpg',
+        address: 'Created during Fall 2021 of LaunchPad Purdue',
+        description:
+          'This app is a planner app that allows you to keep track of your progress and goals you have set. UI used with Dart is Flutter.',
+      },
+];
+
 function PreviousProj() {
     return (
         <div>
-            <h1>Previous projects</h1>
+            <center><h1>Previous projects</h1></center>
+            <center><h3>Fall 2022</h3></center>
+              <div className={classes.container}>
+                <div className = {classes.row}>
+                  <ProjectList projects={DUMMY_DATA} />
+                </div>
+              </div>
+
+            <center><h3>Fall 2021</h3></center>
+              <div className={classes.container}>
+                <div className = {classes.row}>
+                  <ProjectList projects={DUMMY_DATA} />
+                </div>
+              </div>
         </div>
     );
   }
